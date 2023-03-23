bf2c32932ebf3a4bfcb44f502ce5dfe11b2acabc
kingofforeverr <nguyetha1234hn@gmail.com>
2023-03-23 21:03:39 +0700

    .

--- baitapnhom/ham96.py
+++ baitapnhom/ham96.py
@@ -3,9 +3,7 @@ def checkpass(p):
     hoa=False
     thuong=False
     so=False
-    # if len(p)<8:
-    # return False
-    # 
+    
     for ch in p:
         if ch>="A" and ch<="Z":
             hoa=True
@@ -13,7 +11,7 @@ def checkpass(p):
             thuong=True
         elif ch>="0" and ch<="9":
             so=True
-      #    return hoa and thuong and so
+     
     if len(p)>=8 and hoa and thuong and so:
         return True
     return False
@@ -23,7 +21,7 @@ def main():
     if checkpass(p):
         print("Pass ổn đó bro")
     else:
-        print('Pass như cc đổi đi')
+        print('Pass không được tốt lắm đổi đi')
         
 if __name__=="__main__":
     main()
